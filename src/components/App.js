fc6a4529a8984d64d71cd5cb20b5e1b71540cb1f
Uniq23 <kdunique1@gmail.com>
2023-05-03 18:58:00 -0400

    react bootstrap1

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,31 @@
+import { useEffect, useState } from 'react';
 import { Container } from 'react-bootstrap';
+import { ethers } from 'ethers'
 
+// Components
 import Navigation from './Navigation';
 
 function App() {
+  
+  const loadBlockchainData = async () => {
+    // Intiantiate provider
+    const provider = new ethers.providers.Web3Provider(window.ethereum)
+    console.log(provider)
+
+    // Fetch account
+    const accounts = await window.ethereum.request({ method: 'eth_requestAccounts' });
+    console.log(accounts)
+  }
+
+  useEffect(() => {
+      loadBlockchainData()
+  });
+
   return (
     <Container>
       <Navigation />
     </Container>
-    )
+  );
 }
 
 export default App;

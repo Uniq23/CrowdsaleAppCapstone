379496f07ef6bcfea5a7dadeda9e645a76ed0e33
Uniq23 <kdunique1@gmail.com>
2023-05-04 11:06:47 -0400

    Load Contracts errors corrected

--- src/components/App.js
+++ src/components/App.js
@@ -1,29 +1,56 @@
 import { useEffect, useState } from 'react';
 import { Container } from 'react-bootstrap';
-import { ethers } from 'ethers'
+import { ethers } from 'ethers';
 
 // Components
 import Navigation from './Navigation';
+import Info from './Info';
+
+import CROWDSALE_ABI from '../abis/Crowdsale.json';
+import TOKEN_ABI from '../abis/Token.json';
+
+// Config
+import config from '../config.json';
 
 function App() {
+
+  const [provider, setProvider] = useState(null);
+  const [account, setAccount] = useState(null);
+
+  const [isLoading, setIsLoading] = useState(true);
   
   const loadBlockchainData = async () => {
     // Intiantiate provider
     const provider = new ethers.providers.Web3Provider(window.ethereum)
-    console.log(provider)
+    setProvider(provider)
+
+    // Intiantiate contracts
+    const token = new ethers.Contract(config[31337].token.address, TOKEN_ABI, provider)
+    const crowdsale = new ethers.Contract(config[31337].crowdsale.address, CROWDSALE_ABI, provider)
+    setCrowdsale(crowdsale)
+    console.log(token)
 
     // Fetch account
     const accounts = await window.ethereum.request({ method: 'eth_requestAccounts' });
-    console.log(accounts)
+    const account = ethers.utils.getAddress(accounts[0])
+    setAccount(account)
+
+    setIsLoading(false)
+
   }
 
   useEffect(() => {
+    if (isLoading) {
       loadBlockchainData()
-  });
+    }
+  }, [isLoading]);
 
   return (
     <Container>
       <Navigation />
+      <hr />
+      {account && <Info account={account} accountBalance={accountBalance} />}
+      <div>{account}</div>
     </Container>
   );
 }
